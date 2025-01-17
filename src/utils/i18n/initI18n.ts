f0ec21e7a87da32e3cce09b223b8abf9114b3894
Myoxyss <88515460+MyxBow@users.noreply.github.com>
2025-01-17 13:58:41 +0100

    feat: add french translation (#2)

--- src/utils/i18n/initI18n.ts
+++ src/utils/i18n/initI18n.ts
@@ -1,13 +1,17 @@
 import i18next from "i18next";
 
 import enStrings from "../../i18n/en/strings.json";
+import frStrings from "../../i18n/fr/strings.json";
 
 export default function () {
     i18next.init({
-        fallbackLng: "en",
+        fallbackLng: "en, fr",
         resources: {
             en: {
                 strings: enStrings
+            },
+            fr: {
+                strings: frStrings
             }
         },
         defaultNS: "strings",
