9049d93bce6339e472ab8725a31cd5b63ad1f684
GeekCornerGH <45696571+GeekCornerGH@users.noreply.github.com>
2025-01-17 16:44:48 +0100

    fix: game.locale Ik i'm testing in prod

--- src/utils/game/generatePlayerList.ts
+++ src/utils/game/generatePlayerList.ts
@@ -14,8 +14,12 @@ export default async function generatePlayerList(client: Client, game: unoGame):
     // Generate the string
     game.players.forEach(async e => {
         const username = usernames[e];
-        string += t("game.message.embed.playerList", {
-            count: game.state === "waiting" ? 0 : game.cards[e].length, cards: game.state === "inProgress" ? ` - ${game.cards[e].length.toString().padStart(Math.max(...Object.values(game.cards).map(v => v.length.toString().length)), "")}` : "", name: username?.padEnd(Math.max(...Object.values(usernames).map(el => el.length)), ""), plus: game.state === "inProgress" && game.currentPlayer === e ? "+ " : ""
+        string += t("strings:game.message.embed.playerList", {
+            count: game.state === "waiting" ? 0 : game.cards[e].length,
+            cards: game.state === "inProgress" ? ` - ${game.cards[e].length.toString().padStart(Math.max(...Object.values(game.cards).map(v => v.length.toString().length)), "")}` : "",
+            name: username?.padEnd(Math.max(...Object.values(usernames).map(el => el.length)), ""),
+            plus: game.state === "inProgress" && game.currentPlayer === e ? "+ " : "",
+            lng: game.locale
         });
     });
     string += "```";
