f6da136a4ab890c2cb29ffc772c569bef03ae654
GeekCornerGH <45696571+GeekCornerGH@users.noreply.github.com>
2025-01-17 19:54:00 +0100

    fix: Unique Variants name

--- src/utils/game/toTitleCase.ts
+++ src/utils/game/toTitleCase.ts
@@ -1,6 +1,9 @@
 import { t } from "i18next";
 
+import { uniqueVariants } from "../constants.js";
+
 export default (n: string, lng: string) => {
     const [color, variant] = n.split("-");
+    if (uniqueVariants.includes(color as typeof uniqueVariants[number])) return t(`strings:variants.${color}` as any, { lng });
     return t(`strings:colors.${color}` as any, { lng }) + " " + t(`strings:variants.${variant}` as any, { lng });
 };
