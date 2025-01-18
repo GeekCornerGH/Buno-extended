4640070477d1ec32acb0fab52cc21f913c2a551f
GeekCornerGH <45696571+GeekCornerGH@users.noreply.github.com>
2025-01-18 16:59:26 +0100

    fix: Deduplication in viewcards

--- src/interactions/buttons/viewCards.ts
+++ src/interactions/buttons/viewCards.ts
@@ -4,7 +4,6 @@ import { button } from "../../typings/button.js";
 import { runningUnoGame, unoCard } from "../../typings/unoGame.js";
 import { config } from "../../utils/config.js";
 import { ButtonIDs, cardEmojis, cardEmotes } from "../../utils/constants.js";
-import { cardArrayToCount } from "../../utils/game/cardArrayToCount.js";
 import toTitleCase from "../../utils/game/toTitleCase.js";
 
 export const b: button = {
@@ -20,14 +19,10 @@ export const b: button = {
             flags: MessageFlags.Ephemeral,
         });
         const lng = game.locale;
-        const cards = cardArrayToCount(game.cards[interaction.user.id] as unoCard[]);
-        const seenCards = [] as unoCard[];
         return interaction.reply({
             content: game.cards[interaction.user.id].map(card => {
                 const c = card as unoCard;
-                if (seenCards.includes(c)) return undefined;
-                seenCards.push(c);
-                return `${config.emoteless ? `${cardEmotes[c]} ${toTitleCase(c, lng)}${cards[c]! >= 2 ? ` x${cards[c]}` : ""}` : `${cardEmojis[c]}${cards[c]! >= 2 ? ` x${cards[c]!}` : ""}`}` as unoCard;
+                return `${config.emoteless ? `${cardEmotes[c]} ${toTitleCase(c, lng)}` : `${cardEmojis[c]}`}` as unoCard;
             }).filter(v => v !== undefined).join(config.emoteless ? ", " : " "),
             flags: MessageFlags.Ephemeral
         });
