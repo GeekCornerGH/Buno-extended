881fd09a451493de1524e1948853cdd616b6c145
GeekCornerGH <45696571+GeekCornerGH@users.noreply.github.com>
2025-01-19 09:02:16 +0100

    fix: Wild and +4 counting as sab
    Others said it's a bug

--- src/utils/game/playableCard.ts
+++ src/utils/game/playableCard.ts
@@ -1,5 +1,5 @@
 import { unoCard } from "../../typings/unoGame.js";
 
 export default (cards: unoCard[], currentCard: unoCard) => {
-    return cards.filter(c => c.endsWith(currentCard.split("-")[1]) || c === "wild" || c === "+4" || c.startsWith(currentCard.split("-")[0]));
+    return cards.filter(c => c.endsWith(currentCard.split("-")[1]) || c.startsWith(currentCard.split("-")[0]));
 };
